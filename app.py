f0e1edc18a9447659e8362de6e94aedd58671380
mahmed abdallh <medotatch124@gmail.com>
2025-12-06 21:01:11 +0200

    Update file paths for GitHub settings

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ APP_CONFIG = {
     # إعدادات GitHub
     "REPO_NAME": "mahmedabdallh123/Elqds",
     "BRANCH": "main",
-    "FILE_PATH": "elquds2.xlsx",
-    "LOCAL_FILE": "elquds2.xlsx",
+    "FILE_PATH": "l3.xlsx",
+    "LOCAL_FILE": "l3.xlsx",
     
     # إعدادات الأمان
     "MAX_ACTIVE_USERS": 2,
