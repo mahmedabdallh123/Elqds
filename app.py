aead48e90b66321605559c5bdc939eca4c7d2541
mahmed abdallh <medotatch124@gmail.com>
2026-05-02 22:52:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1924,10 +1924,10 @@ with st.sidebar:
             mins, secs = divmod(int(rem.total_seconds()), 60)
             st.success(f"👋 {username} | ⏳ {mins:02d}:{secs:02d}")
         st.markdown("---")
-        if st.button("🔄 تحديث من GitHub"):
+        if st.button("🔄 تحديث  "):
             if fetch_from_github_requests():
                 st.rerun()
-        if st.button("🗑 مسح الكاش"):
+        if st.button("مسح مهملات"):
             st.cache_data.clear()
             st.rerun()
         if st.button("🚪 تسجيل الخروج"):
